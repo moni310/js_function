feb5599aca5b7aeb61fd13051a8a04842001ac36
Moni  Gupta <72276446+moni310@users.noreply.github.com>
2021-11-25 09:50:45 +0530

    Add files via upload

---
+++ ques12.js
@@ -0,0 +1,8 @@
+function my_fun(list1,list2){
+    for (i in list1){
+        sum1=list1[i]+list2[i]
+        console.log(sum1)
+        console.log("--------------------")
+    }
+}
+my_fun([50, 60, 10],[10, 20, 13])
